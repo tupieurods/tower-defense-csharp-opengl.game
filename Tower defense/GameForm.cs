499d9f4dc7bddb174591a1e5e23fecdace1921b9
Elun <Elun@Local_PC>
2012-04-22 18:58:38 +0800

    ????????? ??? ????????? ???? ? GameCoClassLibrary ??????????? ?????????? ????? ?????????? ? ????????

--- Tower defense/GameForm.cs
+++ Tower defense/GameForm.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Windows.Forms;
 using GameCoClassLibrary.Classes;
+using GameCoClassLibrary.Forms;
+using GameCoClassLibrary.Enums;
+using Tower_defense.Properties;
 
 namespace Tower_defense
 {
@@ -15,7 +18,7 @@ namespace Tower_defense
 
     private void MenuNewGame_Click(object sender, EventArgs e)
     {
-      GameConfSelector selectorForm = new GameConfSelector();
+      FormForSelection selectorForm = new FormForSelection(FormType.GameConfiguration);
       if (selectorForm.ShowDialog() == DialogResult.OK)
       {
         if (_game != null)
@@ -23,7 +26,7 @@ namespace Tower_defense
           GameTimer.Stop();
           _game = null;
         }
-        _game = Game.Factory(PBGame, selectorForm.ReturnConfigName());
+        _game = Game.Factory(selectorForm.ReturnFileName(), FactoryAct.Create, GraphicEngineType.WinForms, PBGame);
         if (_game == null)
         {
           Environment.Exit(1);
@@ -32,7 +35,7 @@ namespace Tower_defense
         {
           GameTimer.Interval = 30;//1;
           GameTimer.Start();
-          MessageBox.Show("Game conf loaded successeful");
+          MessageBox.Show(Resources.Game_conf_loaded_successeful);
         }
       }
     }
@@ -64,8 +67,59 @@ namespace Tower_defense
 
     private void GameTimer_Tick(object sender, EventArgs e)
     {
-      _game.Tick();
+      if (!_game.Paused)
+        _game.Tick();
+      if (_game.Lose)
+      {
+        GameTimer.Stop();
+        _game = null;
+        MessageBox.Show(Resources.Gamer_loose);
+        return;
+      }
       _game.Render();
+    }
+
+    private void MenuPause_Click(object sender, EventArgs e)
+    {
+      if (_game == null) return;
+      _game.Paused = !_game.Paused;
+      MenuPause.Text = _game.Paused ? "Unpause" : "Pause";
+    }
+
+    private void MenuSave_Click(object sender, EventArgs e)
+    {
+      if (_game == null) return;
+      FormForSave saveNameForm = new FormForSave();
+      if (saveNameForm.ShowDialog() == DialogResult.OK)
+      {
+        MenuPause_Click(sender, e);
+        _game.SaveGame(saveNameForm.ReturnSaveFileName());
+      }
+    }
+
+    //Переписать когда будет сделан класс меню
+    private void MenuLoad_Click(object sender, EventArgs e)
+    {
+      FormForSelection selectorForm = new FormForSelection(FormType.Load);
+      if (selectorForm.ShowDialog() == DialogResult.OK)
+      {
+        if (_game != null)
+        {
+          GameTimer.Stop();
+          _game = null;
+        }
+        _game = Game.Factory(selectorForm.ReturnFileName(), FactoryAct.Load, GraphicEngineType.WinForms, PBGame);
+        if (_game == null)
+        {
+          Environment.Exit(1);
+        }
+        else
+        {
+          GameTimer.Interval = 30;//1;
+          GameTimer.Start();
+          MessageBox.Show(Resources.Game_loaded_successeful);
+        }
+      }
     }
   }
 }
