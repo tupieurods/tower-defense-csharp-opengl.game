b4d3aa7313d42b973c8448f6b48e59ab9da8640c
Elun <Elun@Local_PC>
2012-06-29 21:43:32 +0800

    ????????? ??? ????? ????????? ????

--- Tower defense/GameForm.cs
+++ Tower defense/GameForm.cs
@@ -2,32 +2,139 @@
 using System.Windows.Forms;
 using GameCoClassLibrary.Classes;
 using GameCoClassLibrary.Enums;
+using GameCoClassLibrary.Forms;
+using GameCoClassLibrary.Interfaces;
+using Button = GameCoClassLibrary.Enums.Button;
+using MainMenu = GameCoClassLibrary.Classes.MainMenu;
+using Menu = GameCoClassLibrary.Classes.Menu;
 
 namespace Tower_defense
 {
   public partial class GameForm : Form
   {
-    private readonly GameMenu _gameMenu;
+    private Menu _gameMenu;
+    private Game _game;
+    private readonly IGraphic _graphObject;
+    private readonly Timer _timer;
+    private float _currentScale;
 
     public GameForm()
     {
       InitializeComponent();
-      _gameMenu = new GameMenu(GraphicEngineType.WinForms, PBGame);
+      _graphObject = new WinFormsGraphic(null);
+      _currentScale = 1.0f;
+      GameResize(1.0f);
+      _gameMenu = new MainMenu(_graphObject);
+      _timer = new Timer();
+      _timer.Tick += TimerTick;
+      _timer.Interval = 30;
+      _timer.Start();
+    }
+
+    private void GameResize(float scaling)
+    {
+      _currentScale = scaling;
+      _graphObject.Resize(0, 0, scaling, PBGame);
+      if (_gameMenu != null)
+      {
+        _gameMenu.Scaling = scaling;
+      }
+      if (_game != null)
+      {
+        _game.Scaling = scaling;
+      }
+    }
+
+    private void CreateNewGame(FormType formType)
+    {
+      FormForSelection selectorForm = new FormForSelection(formType);
+      if (selectorForm.ShowDialog() == DialogResult.OK)
+      {
+        if (_game != null)
+        {
+          if (_timer != null)
+            _timer.Stop();
+          _game = null;
+        }
+        try
+        {
+          _game = Game.Factory(selectorForm.ReturnFileName(),
+                               formType == FormType.GameConfiguration ? FactoryAct.Create : FactoryAct.Load,
+                               _graphObject);
+        }
+        catch
+        {
+          _game = null;
+        }
+        if (_game != null)
+        {
+          _game.Scaling = _currentScale;
+          _gameMenu = null;
+          MessageBox.Show("Game conf loaded successeful");
+        }
+        if (_timer != null)
+          _timer.Start();
+      }
+    }
+
+    private void TimerTick(object obj, EventArgs e)
+    {
+      if (_gameMenu != null)
+      {
+        _gameMenu.Show();
+      }
+      if (_game != null)
+      {
+        _game.Tick(PBGame.PointToClient(Control.MousePosition));
+        _game.Render();
+      }
+      _graphObject.Render();
     }
 
     private void PBGame_MouseUp(object sender, MouseEventArgs e)
     {
+      Button result = Button.Empty;
       if (_gameMenu != null)
       {
-        _gameMenu.MouseUp(e);
+        result = _gameMenu.MouseUp(e);
+        //MessageBox.Show(result.ToString());
+      }
+      if (_game != null)
+      {
+        result = _game.MouseUp(e);
+      }
+      switch (result)
+      {
+        case Button.Empty:
+          break;
+        case Button.BigScale:
+          GameResize(2.0f);
+          break;
+        case Button.NormalScale:
+          GameResize(1.0f);
+          break;
+        case Button.SmallScale:
+          GameResize(0.8125f);
+          break;
+        case Button.Exit:
+          Environment.Exit(0);
+          break;
+        case Button.LoadGame:
+          CreateNewGame(FormType.Load);
+          break;
+        case Button.NewGame:
+          CreateNewGame(FormType.GameConfiguration);
+          break;
+        default:
+          throw new ArgumentOutOfRangeException();
       }
     }
 
     private void PBGame_MouseMove(object sender, MouseEventArgs e)
     {
-      if (_gameMenu != null)
+      if (_game != null)
       {
-        _gameMenu.MouseMove(e);
+        _game.MouseMove(e);
       }
     }
 
